e7033253167585c54841ff525ff4832d69a9eb8c
outfitinspo <23dhunren@gmail.com>
2025-08-21 07:57:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 window.TRACKER_SETTINGS = {
   redirect_url: "https://www.amazon.com?&linkCode=ll2&tag=kymedia0bf-20&linkId=b125944fcbc4c3251aee8c3d2cd0e613&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 0.1,
-  webhook_url: "https://national-regularly-bullfrog.ngrok-free.app/webhook/PT-Tracker",
+  webhook_url: "https://whole-adequate-ghoul.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
   button_text: "View on Amazon",
   disclaimer: "Commission may be earned from the links below with no extra cost to you."
