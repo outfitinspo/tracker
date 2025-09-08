7afa68b1373466f1aa19bd68bbf8c434602636b5
outfitinspo <23dhunren@gmail.com>
2025-09-08 17:51:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 window.TRACKER_SETTINGS = {
-  redirect_url: "https://www.amazon.com/s?k=clothing%2C+shoes+%26+jewelry&i=fashion&crid=2BYP7AXT4NI5Z&sprefix=Clothing%2C+Shoes%2Cfashion%2C663&linkCode=ll2&tag=kymedia00-20&linkId=0aa8945dd80d78d5ae09595fbadaf40d&language=en_US&ref_=as_li_ss_tl",
+  redirect_url: "https://www.amazon.com/s?k=clothing%2C+shoes+%26+jewelry&i=fashion&crid=2BYP7AXT4NI5Z&sprefix=Clothing%2C+Shoes%2Cfashion%2C663&linkCode=ll2&tag=pt-outfitinspo1-20&linkId=cfd3da1efce4f370aa379c0984ddd65a&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 0.1,
   webhook_url: "https://golden-ideally-skunk.ngrok-free.app/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
