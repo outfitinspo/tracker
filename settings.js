abf849585fa64744225ec8194d683fc6a4cda894
outfitinspo <23dhunren@gmail.com>
2025-09-24 17:03:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 window.TRACKER_SETTINGS = {
   redirect_url: "https://www.amazon.com/s?k=clothing%2C+shoes+%26+jewelry&i=fashion&crid=2BYP7AXT4NI5Z&sprefix=Clothing%2C+Shoes%2Cfashion%2C663&linkCode=ll2&tag=pt-outfitinspo1-20&linkId=cfd3da1efce4f370aa379c0984ddd65a&language=en_US&ref_=as_li_ss_tl",
   redirect_delay: 0.1,
-  webhook_url: "https://golden-ideally-skunk.ngrok-free.app/webhook/PT-Tracker",
+  webhook_url: "https://exoticwhollylionfish.brainchain.shop/webhook/PT-Tracker",
   heading_text: "Taking you to the product...",
   button_text: "View on Amazon",
   disclaimer: "Commission may be earned from the links below with no extra cost to you."
